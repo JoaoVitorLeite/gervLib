1602c242b8b3a88d0f184956233be20b4230f0e1
JoaoVitorLeite <joaovitorleite@id.uff.br>
2022-05-20 19:30:44 -0300

    Query

--- Project/rScript/sample_dataset.R
+++ Project/rScript/sample_dataset.R
@@ -1,3 +1,4 @@
+args <- commandArgs(trailingOnly = TRUE)
 
 sample_dataset <- function(filePath, separator, sizeOfSample, sampleFileName = "sample.csv", seed = 100){
   
@@ -6,4 +7,18 @@ sample_dataset <- function(filePath, separator, sizeOfSample, sampleFileName = "
   df <- df[sample(nrow(df), sizeOfSample), ]
   write.table(df, file=paste0(getwd(), "/", sampleFileName), sep=",", row.names = F, col.names = F)
 
+}
+
+if(length(args) == 5){
+  
+  sample_dataset(args[1], args[2], args[3], args[4], args[5])
+  
+} else if(length(args) == 4){
+  
+  sample_dataset(args[1], args[2], args[3], args[4])
+  
+} else if(length(args) == 3){
+  
+  sample_dataset(args[1], args[2], args[3])
+  
 }
